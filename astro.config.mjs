9f2b7e8a39ddc66b1dd48bc95a5575d0730e889c
Jose Calfuen <josecalfuen@gmail.com>
2024-08-01 02:59:54 -0400

    sin base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://jacs626.github.io";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
-  site: "https://jacs626.github.io",
-  base: "portfolio.dev",
+  site: BASE_URL,
   integrations: [tailwind(), robotsTxt()],
 });
