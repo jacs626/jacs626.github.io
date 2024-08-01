6d80a0a0265ffecd4f6c1d896e24b61cff1b1f23
Jose Calfuen <josecalfuen@gmail.com>
2024-07-31 23:36:48 -0400

    da

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://jacs626.github.io",
+  site: "https://jacs626.github.io/porfolio.dev",
 });
